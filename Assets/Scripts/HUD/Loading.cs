06ef5f14b99c23fd04dccebb4a63655bfcdb6b1b
johnchoi313 <johnchoi@andrew.cmu.edu>
2015-07-12 18:02:41 -0400

    Simple Title and Loading Screen

---
+++ Assets/Scripts/HUD/Loading.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loading : MonoBehaviour {
+
+	public string level;
+
+	IEnumerator Start() {
+		AsyncOperation async = Application.LoadLevelAsync(level);
+		yield return async;
+		Debug.Log("Loading complete");
+	}
+}
+
---
+++ Assets/Scripts/HUD/TitleScreen.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreen : MonoBehaviour {
+	
+	public Transform smartphone;
+	public GameObject screen;
+
+	public Texture[] screens;
+	private int screenTex;
+	private bool touched;
+
+	public Vector3 rotAll;
+	public Vector3 rotSpd;
+	public Vector3 rotAmp;
+	
+	void Start () {
+		screenTex = 0;
+	}
+
+	void Update () {
+
+		transform.Rotate(rotAll);
+		Vector3 r = rotSpd*Time.time;
+		smartphone.localEulerAngles = new Vector3(rotAmp.x*Mathf.Sin(r.x),rotAmp.y*Mathf.Sin(r.y),rotAmp.z*Mathf.Sin(r.z));
+
+		//start game upon touch.
+        if(!touched && Input.touchCount > 0 || Input.GetMouseButtonDown(0)) {
+			touched = true;
+			screenTex++;
+			if(screenTex == screens.Length) {
+				Application.LoadLevel("Loading");
+			}
+		} else {
+			touched = false;
+		}
+		//display screen
+		if(0 <= screenTex && screenTex < screens.Length) {
+			screen.GetComponent<Renderer>().material.mainTexture = screens[screenTex];
+		}
+	}	
+}
